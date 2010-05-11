d08644b18e4299e4108f4fbc58786d6af5c8504f
Carlos Fernandez Garcia <carlos.villademor@gmail.com>
2010-05-11 00:58:56 +0000

    Working on jQuery
    git-svn-id: https://fonti.svn.sourceforge.net/svnroot/fonti@53 6c69d8e3-e047-4e8c-8af0-4e4715b4ba7c

--- billing/src/main/webapp/javaScript/functions.js
+++ billing/src/main/webapp/javaScript/functions.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     //Load components for the family selected and change the selected attribute to the family selected
     $("#families").change(function(){
-        $("#families option:selected").removeAttr("selected");
-//        $(this).attr("selected", "selected");
+        //$("#families option").removeAttr("selected");
+        $("#families option:selected").attr("selected", "selected");
     });
 });
